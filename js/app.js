6d9c1d2c61f93a1bfbda54954ee54714c7cbf41b
Your Name <you@example.com>
2022-03-11 21:45:38 +0530

    question add

--- js/app.js
+++ js/app.js
@@ -34,20 +34,26 @@ const switchTab = (id) => {
     if (id === "posts") {
         document.getElementById( "posts" ).style.display = "grid";
         document.getElementById( "liked" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
+        document.getElementById( "reported" ).style.display = "none"; 
         document.getElementById( "bonus" ).style.display = "block";
+      // to show firstpage question 
+
     } else if (id === "liked") {
         document.getElementById( "liked" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
-        document.getElementById( "reported" ).style.display = "none";
-        document.getElementById( "bonus" ).style.display = "none";
+        document.getElementById( "reported" ).style.display = "none"; 
+        document.getElementById( "bonus" ).style.display = "none"; 
+        // to show firstpage question 
+
 
         displayLikedPosts();
     } else {
         document.getElementById( "reported" ).style.display = "block";
         document.getElementById( "posts" ).style.display = "none";
         document.getElementById( "liked" ).style.display = "none";
-        document.getElementById( "bonus" ).style.display = "none";
+        document.getElementById( "bonus" ).style.display = "none";        
+        // to show firstpage question 
+
 
 
         displayReportedPosts();
